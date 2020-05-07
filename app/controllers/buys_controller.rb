7bea56f960f7c7b30a09938f08f2e40df9b6b2b9
fermentedsoybean7110 <62579566+fermentedsoybean7110@users.noreply.github.com>
2020-05-07 11:41:45 +0900

    remove_commentout

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -2,28 +2,24 @@ class BuysController < ApplicationController
 
   require 'payjp'
 
+  before_action :set_item, only: [:index, :pay]
+  before_action :set_card, only: [:index, :pay]
+
   def index
-    @item = Item.find(params[:item_id])
-
-    card = Credit.where(user_id: current_user.id).first
-
-   
+    
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     customer = Payjp::Customer.retrieve(card.customer_id)
     @default_card_information = customer.cards.retrieve(card.card_id)
     
-    
   end
 
   def pay
-    @item = Item.find(params[:item_id])
     unless @item.sold == 2
       @buy = Buy.new(user_id: current_user.id)
       @buy.save!
       @item.sold = 2
       @item.buy_id = @buy.user_id
       @item.save!
-      card = Credit.where(user_id: current_user.id).first
       Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
       Payjp::Charge.create(
       :amount => @item.value, #支払金額を入力（itemテーブル等に紐づけても良い）
@@ -51,4 +47,12 @@ class BuysController < ApplicationController
   #     redirect_to product_path(@product)
   #   end
   # end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+  
+  def set_card
+    card = Credit.find_by(user_id: current_user.id)
+  end
 end
