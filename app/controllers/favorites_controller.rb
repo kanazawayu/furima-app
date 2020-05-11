3994419984e0f6e0950eab1fa3ff3c61efb06f11
kanazawayu <kanazawa975@gmail.com>
2020-05-11 14:14:14 +0900

    fix sat-item

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,18 +1,18 @@
 class FavoritesController < ApplicationController
 
+  before_action :set_item
+
   def create
-    @item = Item.find(params[:item_id])
     @favorite = Favorite.create(user_id: current_user.id, item_id: @item.id)
   end
 
   def destroy
-    @item = Item.find(params[:item_id])
     @favorite = Favorite.find_by(user_id: current_user.id, item_id: @item.id)
     @favorite.destroy
   end
 
   private
-  def favorite_params
-    params.merge(user_id: current_user.id, item_id: @item.id)
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
